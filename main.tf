f0a4fd0b0befbd2b1a6eca954bc95a16d961e2b1
Vitaliy Altukhov <sutinky@gmail.com>
2022-05-02 13:16:20 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,9 @@
-resource "null_resource" "resource3" {
-  #count = 10
+resource "null_resource" "resourceHello" {
+  count = 1500
   provisioner "local-exec" {
     command = "echo $ENV"
     environment = {
       ENV = "Hello World!"
     }
-  }
- 
-  triggers = {
-    test = format("example+%s", timestamp())
   }
 }
