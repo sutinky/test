6e1d8e09c6025dd6a7f810f2ac3d88019c79ab5c
Vitaliy Altukhov <6691862+sutinky@users.noreply.github.com>
2023-05-17 13:18:53 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,26 +1,6 @@
 resource "null_resource" "resourceHello" {
-  count = 2
+  count = 500
   #provisioner "local-exeс" {
     #command = "echo Hello World!"
   #}
 }
-
-variable "false" {
-  default = true
-  sensitive = true
-}
-variable "boolean" {
-  default = true
-  sensitive = true
-}
-
-variable "number" {
-  default = 12
-  sensitive = true
-}
-
-variable "float" {
-  default = 15.55
-  sensitive = true
-}
-
