b4925be7471eea7ebbe6017a4c4a0adfcb32c097
Vitaliy Altukhov <6691862+sutinky@users.noreply.github.com>
2024-01-30 15:53:29 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,26 +5,19 @@ resource "null_resource" "resourceHello" {
   #}
 }
 
+
 variable "thisstr" {
-  type = string
   default = "test"
 }
 
 
 #In this example, the magic_animals_list variable is defined as a list of strings representing magic animals. The resulting list will be ["unicorn", "dragon", "phoenix", "griffin"].
 variable "magic_animals_list" {
-  type    = list(string)
   default = ["unicorn", "dragon", "phoenix", "griffin", "thestral", "cat"]
 }
 
 #In this example, the magic_animals variable is a list of objects. Each object represents a magic animal and contains attributes such as name, power, element, and description. The default value provides a list of four magic animals with their respective attributes.
 variable "magic_animals" {
-  type = list(object({
-    name       = string
-    power      = string
-    element    = string
-    description = string
-  }))
   default = [
     {
       name        = "unicorn"
@@ -56,13 +49,11 @@ variable "magic_animals" {
 
 #In this example, the magic_animals_set variable is defined as a set of strings representing magic animals. The resulting set will be ["unicorn", "dragon", "phoenix", "griffin"]
 variable "magic_animals_set" {
-  type    = set(string)
   default = ["unicorn", "dragon", "phoenix", "griffin", "black cat"]
 }
 
 #In this example, the magic_animals_map variable is defined as a map of strings representing magic animals and their associated qualities. The resulting map will be { "unicorn" = "sparkle", "dragon" = "fire", "phoenix" = "rebirth", "griffin" = "majesty" }.
 variable "magic_animals_map" {
-  type    = map(string)
   default = {
     unicorn  = "none"
     dragon   = "sleep"
@@ -73,11 +64,6 @@ variable "magic_animals_map" {
 
 #In this example, the magic_animal_object variable is defined as an object with three attributes: name, element, and power. The resulting object will be { "name" = "Mermaid", "element" = "Water", "power" = "Enchanting Voice" }.
 variable "magic_animal_object" {
-  type = object({
-    name    = string
-    element = string
-    power   = string
-  })
   default = {
     name    = "MODULE VERSION Small Mermaid"
     element = "Water"
@@ -87,7 +73,6 @@ variable "magic_animal_object" {
 
 #In this example, the magic_animal_tuple variable is defined as a tuple that contains three magical animal attributes: name, element, and power. The resulting tuple will be ["Centaur", "Earth", "Archery"].
 variable "magic_animal_tuple" {
-  type    = tuple([string, string, string])
   default = ["Usual Centaur", "Big Earth", "Non-Archery-Dry-check-on-222staging"]
 }
 
