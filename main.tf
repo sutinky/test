9a18eb3de40851cab92771188bf24aed268c6064
Vitaliy Altukhov <6691862+sutinky@users.noreply.github.com>
2024-05-30 16:08:51 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,31 +16,20 @@ resource "scalr_variable" "example1" {
   description  = "variable description"
   workspace_id = "ws-v0ochom1oljir0mhe"
 }
-/*
-output "instance_ip_addr" {
-  value = "lorem"
-}*/
-
-variable "bool" {
-  type = bool
-  default = false
-}
-
-variable "boolstr" {
-  default = "true"
-}
-variable "str" {
-  default = "test"
-}
-
 
 #In this example, the magic_animals_list variable is defined as a list of strings representing magic animals. The resulting list will be ["unicorn", "dragon", "phoenix", "griffin"].
 variable "magic_animals_list" {
-  default = ["unicorn", "dragon", "phoenix", "griffin", "thestral", "cat"]
+  type    = list(string)
+  default = ["unicorn", "dragon", "phoenix", "griffin", "thestral", "dog"]
 }
-
 #In this example, the magic_animals variable is a list of objects. Each object represents a magic animal and contains attributes such as name, power, element, and description. The default value provides a list of four magic animals with their respective attributes.
 variable "magic_animals" {
+  type = list(object({
+    name       = string
+    power      = string
+    element    = string
+    description = string
+  }))
   default = [
     {
       name        = "unicorn"
@@ -68,15 +57,14 @@ variable "magic_animals" {
     }
   ]
 }
-
-
 #In this example, the magic_animals_set variable is defined as a set of strings representing magic animals. The resulting set will be ["unicorn", "dragon", "phoenix", "griffin"]
 variable "magic_animals_set" {
+  type    = set(string)
   default = ["unicorn", "dragon", "phoenix", "griffin", "black cat"]
 }
-
 #In this example, the magic_animals_map variable is defined as a map of strings representing magic animals and their associated qualities. The resulting map will be { "unicorn" = "sparkle", "dragon" = "fire", "phoenix" = "rebirth", "griffin" = "majesty" }.
 variable "magic_animals_map" {
+  type    = map(string)
   default = {
     unicorn  = "none"
     dragon   = "sleep"
@@ -84,19 +72,35 @@ variable "magic_animals_map" {
     griffin  = "none"
   }
 }
-
 #In this example, the magic_animal_object variable is defined as an object with three attributes: name, element, and power. The resulting object will be { "name" = "Mermaid", "element" = "Water", "power" = "Enchanting Voice" }.
 variable "magic_animal_object" {
+  type = object({
+    name    = string
+    element = string
+    power   = string
+  })
   default = {
     name    = "MODULE VERSION Small Mermaid"
     element = "Water"
     power   = "Enchanting Voice"
   }
 }
-
 #In this example, the magic_animal_tuple variable is defined as a tuple that contains three magical animal attributes: name, element, and power. The resulting tuple will be ["Centaur", "Earth", "Archery"].
 variable "magic_animal_tuple" {
+  type    = tuple([string, string, string])
   default = ["Usual Centaur", "Big Earth", "Non-Archery-Dry-check-on-222staging"]
+}
+
+variable "boolean" {
+  type = bool
+  default = false
+}
+
+variable "true" {
+  default = "true"
+}
+variable "string" {
+  default = "test"
 }
 
 variable "undefined" {
@@ -149,3 +153,7 @@ variable "array_in_object_in_array" {
   ]
 }
 
+/*
+output "instance_ip_addr" {
+  value = "lorem"
+}*/
