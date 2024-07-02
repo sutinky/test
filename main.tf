ef3f16841a573c2d349355eb2fe839b8046d3cf5
Vitaliy Altukhov <6691862+sutinky@users.noreply.github.com>
2024-07-02 19:52:37 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "scalr_variable" "example1" {
   value        = "my_value_name"
   category     = "terraform"
   description  = "variable description"
-  workspace_id = "ws-v0oe4cjptgdtf0ed7"
+  workspace_id = "ws-v0oe4caupa6d60f34"
 }
 /*
 resource "scalr_variable" "magic_animals_list" {
