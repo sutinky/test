956bfa12293e09a2c5cf2bd9a86cba5517eec5de
Vitaliy Altukhov <6691862+sutinky@users.noreply.github.com>
2024-09-03 18:01:53 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,11 @@ output "instance_ip_addr" {
   value = "./test_file_7_special_!@#$%^&*()_+={}|[]\\:\";'<>?,./.txt"
 }
 output "instance_ip_addr2" {
-  value = "\ P This is test file number ${count.index} with special characters TEST TEAM || motor-car |SEPARATE MODULE| \ P"
+  value = "\\ P This is test file number ${count.index} with special characters TEST TEAM || motor-car |SEPARATE MODULE| \\ P"
 }
 
 output "instance_ip_addr3" {
-  value = "\ P This is test file number ${count.index} with special characters TEST TEAM || motor-car |SEPARATE MODULE| \ P"
+  value = "\\ P This is test file number ${count.index} with special characters TEST TEAM || motor-car |SEPARATE MODULE| \\ P"
 }
 /*
 terraform {
