6207964de6e3cc46856749bf8003e3dcd47f0b6e
Vitaliy Altukhov <6691862+sutinky@users.noreply.github.com>
2024-09-24 17:12:17 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 variable "magic_animal_tuple" {
   type    = tuple([string, string, string])
-  default = ["Usual Centaur", "Big Earth1", "Non-Archery-Dry-check-on-222staging"]
+  default = ["Usual Centaur", "Big Earth1", var.string]
 }
 output "magic_animal_tuple" {
   value = var.magic_animal_tuple
@@ -9,40 +9,6 @@ variable "string" {
   type    = string
   default = "123"
   sensitive = true
-}
-variable "magic_animals" {
-  type = list(object({
-    name       = string
-    power      = string
-    element    = string
-    description = string
-  }))
-  default = [
-    {
-      name        = "unicorn"
-      power       = "sparkle"
-      element     = "light"
-      description = "A mystical creature with a single horn on its forehead."
-    },
-    {
-      name        = "dragon"
-      power       = "fire"
-      element     = var.string
-      description = "A powerful creature with the ability to breathe fire."
-    },
-    {
-      name        = "phoenix"
-      power       = "rebirth"
-      element     = "fire"
-      description = "A legendary bird that is reborn from its own ashes."
-    },
-    {
-      name        = "griffin"
-      power       = "majesty"
-      element     = "air"
-      description = "A majestic creature with the body of a lion and the head of an eagle."
-    }
-  ]
 }
 
 
