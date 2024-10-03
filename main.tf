0f4e5895f237e505ff9f883559a1a94714730f06
Vitaliy Altukhov <6691862+sutinky@users.noreply.github.com>
2024-10-03 12:48:05 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,26 @@
+resource "random_uuid" "test" {
+  count = 2
+}
+output "uuid_out" {
+  value = random_uuid.test[*].result
+}
+
+resource "random_password" "password" {
+  count = var.pass_count
+  length           = 16
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+variable "pass_count"{
+  type = string
+  default = "5"
+}
+
+output "password_out" {
+  value = random_password.password[*].result
+sensitive = true
+}
+
 output "with_newline" {
   value = <<-EOT
   LoremLorem
