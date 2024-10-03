cea0a58773e0fddc0db7178dab895a95919833ec
Vitaliy Altukhov <6691862+sutinky@users.noreply.github.com>
2024-10-03 14:02:30 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ output "uuid_array" {
   value = concat(random_uuid.test[*].result, ["lorem"])
 }
 output "uuid_obj" {
-  value = { key0: random_uuid.test[0].result, key1: random_uuid.test[1].result, key2: "lorem" }
+  value = { key0: random_uuid.test[0].result, key1: random_uuid.test[1].result, key2: "lorem", key3: { key0: random_uuid.test[0].result, key1: random_uuid.test[1].result, key2: "lorem" } }
 }
 
 resource "random_password" "password" {
