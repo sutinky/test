a05bfea75f542aead63221ee41e0d7c54861676c
Vitaliy Altukhov <6691862+sutinky@users.noreply.github.com>
2024-10-10 11:42:41 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,10 +8,13 @@ resource "null_resource" "resourceHello" {
 resource "random_uuid" "test" {
   count = 2
 }
-output "uuid_array" {
+output "uuid_array1" {
   value = concat(random_uuid.test[*].result, ["lorem"])
 }
-output "uuid_obj" {
+output "uuid_obj1" {
+  value = { key0: random_uuid.test[0].result, key1: random_uuid.test[1].result, key2: "lorem", key3: { key0: random_uuid.test[0].result, key1: random_uuid.test[1].result, key2: "lorem" } }
+}
+output "uuid_obj2" {
   value = { key0: random_uuid.test[0].result, key1: random_uuid.test[1].result, key2: "lorem", key3: { key0: random_uuid.test[0].result, key1: random_uuid.test[1].result, key2: "lorem" } }
 }
 
