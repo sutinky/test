7c34496308e31f94518531aa54625aa0b47a9722
Vitaliy Altukhov <6691862+sutinky@users.noreply.github.com>
2024-10-15 10:42:00 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ output "uuid_array1" {
   value = concat(random_uuid.test[*].result, ["lorem"])
 }
 output "uuid_obj1" {
-  value = { key0: random_uuid.test[0].result, key1: random_uuid.test[1].result, key2: "lorem", key3: { key0: random_uuid.test[0].result, key1: random_uuid.test[1].result, key2: "lorem1" } }
+  value = { key0: random_uuid.test[0].result, key1: random_uuid.test[1].result, key2: "lorem", key3: { key0: random_uuid.test[0].result, key1: random_uuid.test[1].result, key2: "lorem" } }
 }
 output "uuid_obj2" {
   value = { key0: random_uuid.test[0].result, key1: random_uuid.test[1].result, key2: "lorem", key3: { key0: random_uuid.test[0].result, key1: random_uuid.test[1].result, key2: "lorem" } }
